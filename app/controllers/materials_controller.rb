ffcf74846a6e2df115721aead91e45116b255078
EmilioCenturion <emicenturion13@gmail.com>
2017-02-28 08:50:17 -0400

    Se agrego estilos a CRUD Materiales (#11)

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -28,7 +28,7 @@ class MaterialsController < ApplicationController
 
     respond_to do |format|
       if @material.save
-        format.html { redirect_to @material, notice: 'Material was successfully created.' }
+        format.html { redirect_to @material, notice: 'Has creado el registro con exito.' }
         format.json { render :show, status: :created, location: @material }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class MaterialsController < ApplicationController
   def update
     respond_to do |format|
       if @material.update(material_params)
-        format.html { redirect_to @material, notice: 'Material was successfully updated.' }
+        format.html { redirect_to @material, notice: 'Has modificado el registro con exito.' }
         format.json { render :show, status: :ok, location: @material }
       else
         format.html { render :edit }
@@ -56,19 +56,19 @@ class MaterialsController < ApplicationController
   def destroy
     @material.destroy
     respond_to do |format|
-      format.html { redirect_to materials_url, notice: 'Material was successfully destroyed.' }
+      format.html { redirect_to materials_url, notice: 'Has eliminido el registro con exito.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_material
-      @material = Material.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_material
+    @material = Material.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def material_params
-      params.require(:material).permit(:nombre, :descripcion, :unidad_medida, :cantidad, :precio, :stock_minimo)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def material_params
+    params.require(:material).permit(:nombre, :descripcion, :unidad_medida, :cantidad, :precio, :stock_minimo)
+  end
 end
