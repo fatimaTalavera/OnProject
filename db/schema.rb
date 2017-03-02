bab7314ed1fa359de2de69452aa2e0ac54b14acd
ergo <stratofor@gmail.com>
2017-03-01 21:56:27 -0300

    Creado los archivos para el CRUD de clientes con scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301213937) do
+ActiveRecord::Schema.define(version: 20170302004743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "last_name"
+    t.integer  "identification_document"
+    t.string   "email"
+    t.string   "adress"
+    t.string   "phone"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "material_movements", force: :cascade do |t|
     t.date     "fecha"
