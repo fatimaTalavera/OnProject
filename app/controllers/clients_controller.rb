5ebbe9a4e92cda6d04541d3a7e7b1be2ad9ee51f
ergo <stratofor@gmail.com>
2017-03-02 00:46:24 -0300

    Se agrego estilo al CRUD de clientes

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -28,7 +28,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.html { redirect_to @client, notice: 'El cliente se creo correctamente.' }
         format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ClientsController < ApplicationController
   def update
     respond_to do |format|
       if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+        format.html { redirect_to @client, notice: 'El cliente se actualizo correctamente.' }
         format.json { render :show, status: :ok, location: @client }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class ClientsController < ApplicationController
   def destroy
     @client.destroy
     respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
+      format.html { redirect_to clients_url, notice: 'Cliente se elimino correctamente.' }
       format.json { head :no_content }
     end
   end
