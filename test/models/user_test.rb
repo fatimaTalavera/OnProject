c3c64c3ff44e9d9b3e1593d8f688828c121bd234
Javier Rios Selent <rsjavier14@gmail.com>
2017-03-18 17:16:43 -0300

    Add test for user. Closes #34

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Users count" do
+    assert_equal 1, User.count
+  end
+
+  test "Find a user" do
+    assert_equal "first_name", users(:one).first_name
+  end
+
+  test "Should not save user without data" do
+    user = User.new()
+    assert_not user.save, "Saved the user without a data"
+  end
+
+  test "Should not save a user without first_name and last_name" do
+    user = User.new(email: "email@email.com", encrypted_password: "12341234")
+    assert_not user.save, "Saved the user without a first_name and last_name"
+  end
+
+  test "Should not save a user without email" do
+    user = User.new(first_name: "name", last_name: "last_name", encrypted_password: "12341234")
+    assert_not user.save, "Saved the user without a email"
+  end
+
+  test "Should not save a user with an email that already exists" do
+    user = User.new(email: "one@email.com", encrypted_password: "12341234", first_name: "name", last_name: "last_name")
+    assert_not user.save, "Saved the user with an email already exists"
+  end
+
 end
