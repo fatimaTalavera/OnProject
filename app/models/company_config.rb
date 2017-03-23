0c4008874306f8068201cb9e5d2c2774c2fbc328
Javier Rios Selent <rsjavier14@gmail.com>
2017-03-22 21:54:58 -0300

    Agrego ruta y modelo para la configuracion

---
+++ app/models/company_config.rb
@@ -0,0 +1,3 @@
+class CompanyConfig < ApplicationRecord
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
   resources :users
+  resources :company_config, only: [:index, :edit, :update]
 end
