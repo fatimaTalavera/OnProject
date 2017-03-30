b162cc03c694c6a3990daa5ac19edca33878d01c
alexjw <abel.oalex@gmail.com>
2017-03-30 02:03:35 -0300

    Creados mas seeds, y arreglados detalles

--- app/controllers/internal_certifications_controller.rb
+++ app/controllers/internal_certifications_controller.rb
@@ -29,7 +29,7 @@ class InternalCertificationsController < ApplicationController
 
     respond_to do |format|
       if @internal_certification.save
-        format.html { redirect_to @internal_certification, notice: 'Internal certification was successfully created.' }
+        format.html { redirect_to internal_certifications_path, notice: 'Internal certification was successfully created.' }
         format.json { render :show, status: :created, location: @internal_certification }
       else
         format.html { render :new }
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,64 @@ if CompanyConfig.first.nil?
   @company_config = CompanyConfig.new name: 'ON', address: 'Encarnación', tel_fax: '(000) 000 000', celphone: '(0000) 000 000', email: 'email@email.com', avatar: ''
   @company_config.save!
 end
+
+# @admin = User.new(name: 'admin', email: 'admin@admin.com', password: "12341234")
+# @admin.save
+
+# Clientes
+@client = Client.new(name: "Alex", last_name: "Jines", ruc: 4159522, email: "abel.oalex@gmail.com", adress: "Calle Zoilo San Miguel C/ Gregorio Domanmchuk", phone:"0985833038")
+@client.save
+
+# Contratos
+@contract = Contract.new(client_id: @alex.id, start_date: Date.today, end_date: Date.today, name: "Un mundo Feliz", amount: 5000 )
+@contract.save
+
+# Materiales
+@cemento = Material.new(name: "Cemento", description: "Bolsas de cemento", price: 50000, quantity: 10, minimun_stock: 1, measurement_unit: "Kg")
+@cemento.save
+@cable = Material.new(name: "Cable", description: "50 mm", price: 10000, quantity: 10, minimun_stock: 1, measurement_unit: "m")
+@cable.save
+@clavo = Material.new(name: "Clavo", description: "10 cm", price: 1000, quantity: 200, minimun_stock: 20, measurement_unit: "u")
+@clavo.save
+
+# Proveedores
+@provider = Provider.new(name: "Inpaco", address: "Avenida Irrazabal", phone: "205012", ruc: "1234456-1", email: "inpaco@test.com", state: TRUE, balance: 0)
+@provider.save
+@provider2 = Provider.new(name: "Vallemi", address: "Villeta km 34", phone: "0985328593", ruc: "12945-1", email: "vallemi@test.com", state: TRUE, balance: 0)
+@provider2.save
+
+# Jornaleros
+@employee = Employee.new(name: "Jorge", last_name: "Esquivel", address: "San Juan del Parana", phone: "0985222333", identification_document: "4393032", email: "jorgenoob123@gmail.com")
+@employee.save
+
+# Servicios
+@service = Service.new(name: "Instalacion de cables", price: 1000000, description: "Se instalan los cables", measurement_unit: "m")
+@service.save
+
+# Movimientos de materiales
+@materialMovement = MaterialMovement.new(fecha: '01/01/2000', contract_id: @contract.id, motivo: "Lecherado para la bicicenda, tramo 1")
+@materialMovement.save
+
+# Movimientos detalles
+@movementDetail = MovementDetail.new(cantidad: 3, material_id: @cemento.id, material_movement_id: @materialMovement.id)
+@movementDetail.save
+@movementDetail = MovementDetail.new(cantidad: 1, material_id: @cable.id, material_movement_id: @materialMovement.id)
+@movementDetail.save
+@movementDetail = MovementDetail.new(cantidad: 150, material_id: @clavo.id, material_movement_id: @materialMovement.id)
+@movementDetail.save
+
+# Facturas de compra
+@purchaseBill = PurchaseBill.new(number: 123, provider_id: @provider.id, date: '30/03/2017')
+@purchaseBill.save
+
+# Facturas detalle
+@purchaseDetail = PurchaseDetail.new(material_id: @clavo.id, quantity: 300, purchase_bill_id: @purchaseBill.id, price: 100)
+@purchaseDetail.save
+@purchaseDetail = PurchaseDetail.new(material_id: @cable.id, quantity: 30, purchase_bill_id: @purchaseBill.id, price: 6000)
+@purchaseDetail.save
+@purchaseDetail = PurchaseDetail.new(material_id: @cemento.id, quantity: 8, purchase_bill_id: @purchaseBill.id, price: 25000)
+@purchaseDetail.save
+
+# Certificaciones internas
+@internalCertification = InternalCertification.new(employee_id: @employee.id, contract_id: @contract.id, date: '30-03-2017', amount: 300000)
+@internalCertification.save
