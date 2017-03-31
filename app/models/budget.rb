cbd3148cb0e43f820aa00a8833dcea2453f3b02a
Emilio Centurion <emicenturion13@gmail.com>
2017-03-31 02:18:43 -0400

    modificaciones en vistas y controller de presupuesto

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -6,4 +6,14 @@ class Budget < ApplicationRecord
   accepts_nested_attributes_for :budget_service_details, allow_destroy: true
   accepts_nested_attributes_for :budget_material_details, allow_destroy: true
 
+  after_create :ac_total_amount
+
+  def ac_total_amount
+    total_amount = 0
+    self.budget_service_details.each do |service_detail|
+      service = Service.find(service_detail.service_id)
+      total_amount = total_amount + (service.price * service_detail.service_quantity)
+    end
+      self.total_amount = total_amount
+  end
 end
