866372671ede436878337f806a6b99783fa8ccb1
Emilio Centurion <emicenturion13@gmail.com>
2017-04-04 00:48:20 -0400

    resolucion conflicto

--- app/models/budget_service_detail.rb
+++ app/models/budget_service_detail.rb
@@ -4,13 +4,13 @@ class BudgetServiceDetail < ApplicationRecord
 
   after_create :ac_subtotal
 
-def ac_subtotal
+  validates :service_id, :presence => {:message => "Seleccione servicio"}
+  validates :service_quantity, :presence => {:message => "No puede estar en blanco"}
 
-  service = Service.find(self.service_id)
-  subtotal = service.price * service_quantity
-
-  self.subtotal = subtotal
-end
-
+  def ac_subtotal
+    service = Service.find(self.service_id)
+    subtotal = service.price * service_quantity
+    self.update_attributes(subtotal: subtotal)
+  end
 
 end
