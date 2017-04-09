a5a7766d23ebe8899b59490b90145c5d8788e9cb
Javier Rios Selent <rsjavier14@gmail.com>
2017-04-09 19:38:32 -0400

    Agrego maximos y minimos de caracteres para usuarios. Closes #85

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :confirmable,
          :recoverable, :rememberable, :trackable
 
-  validates :first_name, presence: true, length: {maximum: 30, minimum: 1}
-  validates :last_name, presence: true, length: {maximum: 30, minimum: 1}
+  validates :first_name, presence: true, length: {maximum: 40, minimum: 2}
+  validates :last_name, presence: true, length: {maximum: 40, minimum: 2}
 
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: :email_format}
-  validates :password, presence: true, confirmation: true, on: :create
-  validates :password, length: {maximum: 15, minimum: 8}, on: :create
-  validates :password, length: {maximum: 12, minimum: 8}, on: :update, allow_blank: true
+  validates :email, presence: true, length: {maximum: 40}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: :email_format}
+  validates :password, presence: true, length: {maximum: 30, minimum: 8}, confirmation: true, on: :create
+  validates :password, length: {maximum: 30, minimum: 8}, on: :create
+  validates :password, length: {maximum: 30, minimum: 8}, on: :update, allow_blank: true
   validates_uniqueness_of :email, allow_blank: false
   validates :role_id , presence: true
   validate :avatar_size_validation
