c7f5c178a03c2ebb7208c9840d3679682a433abd
Javier Rios Selent <rsjavier14@gmail.com>
2017-04-12 19:12:35 -0400

    Soluciono el problema para editar los permisos. Refs #118

--- app/models/models.rb
+++ app/models/models.rb
@@ -1,16 +1,16 @@
 class Models
   include Ruby::Enum
-  define :Role, "Roles"
-  define :User, "Usuarios"
   define :Client, "Clientes"
+  define :ClientCertification, "Certificación Externa"
+  define :CompanyConfig, "Datos de la empresa"
+  define :Contract, "Contratos"
   define :Employee, "Empleados"
   define :InternalCertification, "Certificación Interna"
-  define :ClientCertification, "Certificación Externa"
-  define :Contract, "Contratos"
   define :Material, "Materiales"
   define :MaterialMovement, "Mov. de materiales"
   define :Provider, "Proveedores"
-  define :Service, "Servicios"
   define :PurchaseBill, "Factura de compras"
-  define :CompanyConfig, "Datos de la empresa"
+  define :Role, "Roles"
+  define :Service, "Servicios"
+  define :User, "Usuarios"
 end
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,3 +1,4 @@
 class Permission < ApplicationRecord
+  default_scope { order(subject_class: :asc) }
   belongs_to :role
 end
