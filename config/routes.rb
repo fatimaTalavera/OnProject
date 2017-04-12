36d2e1c7ed0ba060181cc3bffdf5647f42c099e3
Javier Rios Selent <rsjavier14@gmail.com>
2017-04-11 20:51:57 -0400

    Soluciono problema con las rutas para perfil de usuario.

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
   resources :material_movements
   resources :roles, except: [:destroy]
   resources :permissions, only: [:create, :update, :destroy]
-  devise_for :users
-  resources :users
   root to: 'welcome#index'
   get 'welcome/index'
   get '/users/me', to: 'users#edit_profile', as: 'edit_profile'
   post '/users/me', to: 'users#update_profile', as: 'update_profile'
+  devise_for :users
+  resources :users
   get 'company_config', to: 'company_config#edit_company', as: 'edit_company'
   post 'company_config', to: 'company_config#update_company', as: 'update_company'
 end
