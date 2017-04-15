a459dba343577ec6885b9baef8737edb8915c46d
alexjw <abel.oalex@gmail.com>
2017-04-15 12:39:37 -0400

    Arreglados los bugs #112, #113, #114

--- app/models/rubro_service_detail.rb
+++ app/models/rubro_service_detail.rb
@@ -7,7 +7,7 @@ class RubroServiceDetail < ApplicationRecord
   validates_numericality_of :quantity, :presence => {:message => "Debe rellenar este campo"},
                             :greater_than_or_equal_to => 1,
                             :less_than_or_equal_to => 9999,
-                            :message => "Ingrese un número menor a 9999"
+                            :message => "Ingrese un número positivo menor a 9999"
 
   validates :service_id, :presence => {:message => "Debe seleccionar un servicio"}
 
