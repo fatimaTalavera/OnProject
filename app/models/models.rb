3474612d441b1d625f76b1078dc49b766dfda207
Javier Rios Selent <rsjavier14@gmail.com>
2017-04-21 00:32:48 -0400

    Soluciono errores en roles y permisos. Refs #160

--- app/models/models.rb
+++ app/models/models.rb
@@ -1,12 +1,12 @@
 class Models
   include Ruby::Enum
   define :Budget, "Presupuestos"
+  define :Certification, "Certificación Interna"
   define :Client, "Clientes"
   define :ClientCertification, "Certificación Externa"
   define :CompanyConfig, "Datos de la empresa"
   define :Contract, "Contratos"
   define :Employee, "Empleados"
-  define :Certification, "Certificación Interna"
   define :Material, "Materiales"
   define :MaterialMovement, "Mov. de materiales"
   define :Provider, "Proveedores"
