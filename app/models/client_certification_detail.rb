613e57926f821da834ff8dac82531e23e3584037
ergo <stratofor@gmail.com>
2017-05-01 21:19:52 -0400

    Popover y modificaciones en la vista de Certificaciones externas

--- app/models/client_certification_detail.rb
+++ app/models/client_certification_detail.rb
@@ -3,6 +3,8 @@ class ClientCertificationDetail < ApplicationRecord
   belongs_to :client_certification
   belongs_to :rubro
 
+  delegate :name, to: :rubro, prefix: true
+
   validates :rubro_id, :presence => {:message => "Seleccione un rubro"}
   validates :quantity, :presence => {:message => "No puede estar vacio"},
                         :numericality => { :greater_than_or_equal_to => 0, message: "No puede ser negativo"}
