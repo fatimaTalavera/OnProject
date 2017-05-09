5137021e9f2f784abbbc378cbd521a281a876c57
fatimaTalavera <fa.talavera95@gmail.com>
2017-05-09 00:51:28 -0400

    Formato fecha en movimiento de materiales #188

--- app/assets/javascripts/libs/defaults.configs.js
+++ app/assets/javascripts/libs/defaults.configs.js
@@ -1,7 +1,7 @@
 // configuraciones por default para datepicker
 $.fn.datepicker.defaults.language = 'es'; // datepickeren español
 $.fn.datepicker.defaults.autoclose = true; // que se cierre luego de elegir una fecha
-$.fn.datepicker.defaults.format = 'dd/mm/yy'; // formato por defecto
+$.fn.datepicker.defaults.format = 'dd/mm/yyyy'; // formato por defecto
 $.fn.datepicker.defaults.todayHighlight = true;
 $.fn.datepicker.defaults.startDate = moment([1900]).toDate(); // Prevenir que se seleccione fecha cero
 
