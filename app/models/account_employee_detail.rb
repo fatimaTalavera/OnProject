ead54cc9da87e673af2ab7aeccf48a9cec477605
Tania Monges <taniamonges@gmail.com>
2017-05-28 23:14:09 -0400

    se arregla buscador cta cte jornalero, se agrega show

--- app/models/account_employee_detail.rb
+++ app/models/account_employee_detail.rb
@@ -1,5 +1,6 @@
 class AccountEmployeeDetail < ApplicationRecord
   belongs_to :account_employee
   belongs_to :employee
+  delegate :name_with_initial, :identification_document, to: :employee, prefix: true
 
 end
