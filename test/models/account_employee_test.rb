643d1edcab7c77d2f8fef8c2dced3a1899f7f059
Tania Monges <taniamonges@gmail.com>
2017-05-29 15:29:16 -0400

    se agrega unit test cta cte jornalero y se arregla unit test certificacion interna

--- test/models/account_employee_test.rb
+++ test/models/account_employee_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class AccountEmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save account employee without data" do
+    account_employee = AccountEmployee.new()
+    assert_not account_employee.valid?, "Cannot save account employee without data. Errors: #{account_employee.errors.to_a.to_sentence}"
+  end
+
+  test "Should not save account employee without date" do
+    account_employee = AccountEmployee.new(pay:0, contract_id:1)
+    assert_not account_employee.valid?, "Cannot save account employee without date. Errors: #{account_employee.errors.to_a.to_sentence}"
+  end
+
+  test "Should not save account employee without contract" do
+    account_employee = AccountEmployee.new(pay:0)
+    assert_not account_employee.valid?, "Cannot save account employee without contract. Errors: #{account_employee.errors.to_a.to_sentence}"
+  end
 end
