de9187ef5396e74c8c4c347307c21e1f96530c13
fatimaTalavera <fa.talavera95@gmail.com>
2017-06-10 04:05:12 -0400

    se agrego unidad de medida a rubro y formato de montos

--- app/assets/javascripts/helpers/TableHelper.js
+++ app/assets/javascripts/helpers/TableHelper.js
@@ -1,74 +1,75 @@
 function checkPrice(price) {
-  return isNaN(price) ? 0 : price;
+    return isNaN(price) ? 0 : price;
 }
 var TableHelper = {
-    rowsClicksEvent: function() {
-        $('body').on('click','tr.row-click', function(e){
+    rowsClicksEvent: function () {
+        $('body').on('click', 'tr.row-click', function (e) {
             window.location = $(this).data('url');
         });
     },
 
-    totalsEvent: function(options) {
-      var table = options.table;
-      $(table).on('change', options.selector, function(){
-        var row = $(this).parents('tr');
-        var last_row = $(table).find("tr").last().find('.table-total');
-        $.ajax({
-          url: options.url + row.find(options.selector).val(),
-          type: "GET",
-          dataType: "json"
-        }).done(function(data) {
-          var total = NumberHelper.aNumero(last_row.text());
-          row.find('.unit-price').val(data.price);
-          var price = NumberHelper.aNumero(row.find('.unit-price').val());
-          isNaN(price) ? 0 : price;
-          var qty = parseFloat(row.find('.qty').val());
-          var subtotal = NumberHelper.aNumero(row.find('.subtotal').text());
-          total -= subtotal;
-          var subtotal = price * (isNaN(qty) ? 1 : qty);
-          row.find('.subtotal').text(I18n.toCurrency(subtotal, {unit: ''}));
-          total += parseFloat(subtotal);
-          last_row.text(NumberHelper.aMoneda(total));
-          $('.input_total').val(total);
-        }).fail(function() {
-          alert("Ha ocurrido un error inesperado.");
+    totalsEvent: function (options) {
+        var table = options.table;
+        $(table).on('change', options.selector, function () {
+            var row = $(this).parents('tr');
+            var last_row = $(table).find("tr").last().find('.table-total');
+            $.ajax({
+                url: options.url + row.find(options.selector).val(),
+                type: "GET",
+                dataType: "json"
+            }).done(function (data) {
+                var total = NumberHelper.aNumero(last_row.text());
+                if (data.hasOwnProperty('measurement_unit')) row.find('.measurement').val(data.measurement_unit);
+                row.find('.unit-price').val(data.price);
+                var price = NumberHelper.aNumero(row.find('.unit-price').val());
+                isNaN(price) ? 0 : price;
+                var qty = parseFloat(row.find('.qty').val());
+                var subtotal = NumberHelper.aNumero(row.find('.subtotal').text());
+                total -= subtotal;
+                var subtotal = price * (isNaN(qty) ? 1 : qty);
+                row.find('.subtotal').text(I18n.toCurrency(subtotal, {unit: ''}));
+                total += parseFloat(subtotal);
+                last_row.text(NumberHelper.aMoneda(total));
+                $('.input_total').val(total);
+            }).fail(function () {
+                alert("Ha ocurrido un error inesperado.");
+            });
         });
-      });
 
-      $(table).on('keyup', '.qty, .unit-price', function(){
-        var row = $(this).parents('tr');
-        var last_row = $(table).find("tr").last().find('.table-total');
-        $.ajax({
-          url: options.url + row.find(options.selector).val(),
-          type: "GET",
-          dataType: "json"
-        }).done(function(data) {
-          var total = NumberHelper.aNumero(last_row.text());
-          var price = NumberHelper.aNumero(row.find('.unit-price').val());
-          //isNaN(price) ? row.find('.unit-price').val(0) : price;
-          var qty = parseFloat(row.find('.qty').val());
-          var subtotal = NumberHelper.aNumero(row.find('.subtotal').text());
-          total -= subtotal;
-          subtotal = price * (isNaN(qty) ? 1 : qty);
-          isNaN(qty) ? (row.find('.qty').val(1)) : qty;
-          row.find('.subtotal').text(I18n.toCurrency(subtotal, {unit: ''}));
-          total += parseFloat(subtotal);
-          last_row.text(NumberHelper.aMoneda(total));
-          $('.input_total').val(total);
-        }).fail(function() {
-          alert("Ha ocurrido un error inesperado.");
+        $(table).on('keyup', '.qty, .unit-price', function () {
+            var row = $(this).parents('tr');
+            var last_row = $(table).find("tr").last().find('.table-total');
+            $.ajax({
+                url: options.url + row.find(options.selector).val(),
+                type: "GET",
+                dataType: "json"
+            }).done(function (data) {
+                var total = NumberHelper.aNumero(last_row.text());
+                var price = NumberHelper.aNumero(row.find('.unit-price').val());
+                //isNaN(price) ? row.find('.unit-price').val(0) : price;
+                var qty = parseFloat(row.find('.qty').val());
+                var subtotal = NumberHelper.aNumero(row.find('.subtotal').text());
+                total -= subtotal;
+                subtotal = price * (isNaN(qty) ? 1 : qty);
+                isNaN(qty) ? (row.find('.qty').val(1)) : qty;
+                row.find('.subtotal').text(I18n.toCurrency(subtotal, {unit: ''}));
+                total += parseFloat(subtotal);
+                last_row.text(NumberHelper.aMoneda(total));
+                $('.input_total').val(total);
+            }).fail(function () {
+                alert("Ha ocurrido un error inesperado.");
+            });
         });
-      });
 
-      $(table).on('click', '.remove_nested_fields', function(){
-        var row = $(this).parents('tr');
-        var last_row = $(table).find("tr").last().find('.table-total');
-        var total = parseFloat(NumberHelper.aNumero(last_row.text()));
-        var subtotal = parseFloat(NumberHelper.aNumero(row.find('.subtotal').text()));
-        total -= subtotal;
-        last_row.text(NumberHelper.aMoneda(total));
-        $('.input_total').val(total);
-      });
+        $(table).on('click', '.remove_nested_fields', function () {
+            var row = $(this).parents('tr');
+            var last_row = $(table).find("tr").last().find('.table-total');
+            var total = parseFloat(NumberHelper.aNumero(last_row.text()));
+            var subtotal = parseFloat(NumberHelper.aNumero(row.find('.subtotal').text()));
+            total -= subtotal;
+            last_row.text(NumberHelper.aMoneda(total));
+            $('.input_total').val(total);
+        });
 
     }
 };
--- app/assets/javascripts/modules/rubro.js
+++ app/assets/javascripts/modules/rubro.js
@@ -1,49 +1,61 @@
-var Rubro = (function(){
+var Rubro = (function () {
 
-  function initFormEvents(){
-    Material.search({selector: elements.materialSearch, url: setSearchMaterialUrl});
-    Material.search({selector: elements.serviceSearch, url: searchServiceUrl});
-    $("#material-details-body").on("nested:fieldAdded", function(event, insertedItem) {
-      //$(event.target).find(':input').enableClientSideValidations();
-      var name_field = event.field.find(elements.materialSearch.selector);
-      Material.search($.extend({selector: name_field.val($(event.link).data('predefined-name')), url: setSearchMaterialUrl}));
-    });
-    $("#service-details-body").on("nested:fieldAdded", function(event, insertedItem) {
-      //$(event.target).find(':input').enableClientSideValidations();
-      var name_field = event.field.find(elements.serviceSearch.selector);
-      Material.search($.extend({selector: name_field.val($(event.link).data('predefined-name')), url: searchServiceUrl}));
-    });
-  }
-
-  return {
-    init: function() {
-      elements =
-        {
-          materialSearch: $('.material-search'),
-          serviceSearch: $('.service-search'),
-        };
-    },
-    index: function() {
-      DatepickerHelper.initDateRangePicker('#date-range');
-    },
-    'new': function() {
-      initFormEvents();
-    },
-    'create': function(){
-      initFormEvents();
-    },
-    'edit': function() {
-      initFormEvents();
-    },
-    'update': function(){
-      initFormEvents();
-    },
-    setSearchMaterial: function(url) {
-      setSearchMaterialUrl = url;
-    },
-    setSearchService: function(url) {
-      searchServiceUrl = url;
+    function initFormEvents() {
+        Material.search({selector: elements.materialSearch, url: setSearchMaterialUrl});
+        Material.search({selector: elements.serviceSearch, url: searchServiceUrl});
+        TableHelper.totalsEvent({selector: elements.serviceSearch.selector, table: elements.serviceTable.selector, url: '/services/'});
+        TableHelper.totalsEvent({selector: elements.materialSearch.selector, table: elements.materialTable.selector, url: '/materials/'});
+        $("#material-details-body").on("nested:fieldAdded", function (event, insertedItem) {
+            //$(event.target).find(':input').enableClientSideValidations();
+            NumberHelper.mascaraMoneda('.maskMoneda');
+            var name_field = event.field.find(elements.materialSearch.selector);
+            Material.search($.extend({
+                selector: name_field.val($(event.link).data('predefined-name')),
+                url: setSearchMaterialUrl
+            }));
+        });
+        $("#service-details-body").on("nested:fieldAdded", function (event, insertedItem) {
+            //$(event.target).find(':input').enableClientSideValidations();
+            NumberHelper.mascaraMoneda('.maskMoneda');
+            var name_field = event.field.find(elements.serviceSearch.selector);
+            Material.search($.extend({
+                selector: name_field.val($(event.link).data('predefined-name')),
+                url: searchServiceUrl
+            }));
+        });
     }
-  };
+
+    return {
+        init: function () {
+            elements =
+            {
+                serviceTable: $('#service-table'),
+                materialTable: $('#material-table'),
+                materialSearch: $('.material-search'),
+                serviceSearch: $('.service-search'),
+            };
+        },
+        index: function () {
+            DatepickerHelper.initDateRangePicker('#date-range');
+        },
+        'new': function () {
+            initFormEvents();
+        },
+        'create': function () {
+            initFormEvents();
+        },
+        'edit': function () {
+            initFormEvents();
+        },
+        'update': function () {
+            initFormEvents();
+        },
+        setSearchMaterial: function (url) {
+            setSearchMaterialUrl = url;
+        },
+        setSearchService: function (url) {
+            searchServiceUrl = url;
+        }
+    };
 
 }());
